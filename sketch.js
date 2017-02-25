ec18a48fba7c735c5658a49a259b177bcead6e86
ramossitompul <ramos022017@gmail.com>
2017-02-25 15:13:31 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   var x = 0;
   for (var i = 0; i < width / symbolSize; i++) {
     var stream = new Stream();
-    stream.generateSymbols(x, random(-9000, 0));
+    stream.generateSymbols(x, random(-1000, 0));
     streams.push(stream);
     x += symbolSize
   }
